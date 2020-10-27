c37f3ea901bd867516d65d0c013e1946ae7e5ea7
FelipeHamannAndrade <67886006+FelipeHamannAndrade@users.noreply.github.com>
2020-10-27 12:35:36 -0300

    Add files via upload

---
+++ HelloWorldHtml.java
@@ -0,0 +1,18 @@
+package br.com.usj.programacao2.helloworldhtml;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+public class HelloWorldHtml {
+
+    @GetMapping(value="/hello")
+    public ModelAndView getHelloWorldHtml () {
+        ModelAndView modelAndView = new ModelAndView("hello-world");
+        return modelAndView;
+    }
+    
+
+}
---
+++ HelloWorldHtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.usj.programacao2.helloworldhtml;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldHtmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
